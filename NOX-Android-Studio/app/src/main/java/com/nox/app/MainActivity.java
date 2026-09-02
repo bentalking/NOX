c60479c0b6a94bd4b5edcff52916ca653a6825c7
bentalking <130409445+bentalking@users.noreply.github.com>
2026-09-02 21:58:33 +0200

    Improve WebView settings for local assets + mixed content + better error resilience

--- NOX-Android-Studio/app/src/main/java/com/nox/app/MainActivity.java
+++ NOX-Android-Studio/app/src/main/java/com/nox/app/MainActivity.java
@@ -6,16 +6,19 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.view.Window;
-import android.view.WindowInsets;
 import android.graphics.Color;
+import android.util.Log;
 
 public class MainActivity extends Activity {
     private WebView webView;
     private static final int CAMERA_REQUEST = 42;
+    private static final String TAG = "NOX";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +45,35 @@ public class MainActivity extends Activity {
         s.setDatabaseEnabled(true);
         s.setAllowFileAccess(true);
         s.setAllowContentAccess(true);
+        s.setAllowFileAccessFromFileURLs(true);
+        s.setAllowUniversalAccessFromFileURLs(true);
         s.setMediaPlaybackRequiresUserGesture(false);
         s.setBuiltInZoomControls(false);
         s.setDisplayZoomControls(false);
         s.setSupportZoom(false);
+        s.setLoadWithOverviewMode(true);
+        s.setUseWideViewPort(true);
+        s.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        if (android.os.Build.VERSION.SDK_INT >= 26) {
+            s.setSafeBrowsingEnabled(false);
+        }
         s.setUserAgentString(s.getUserAgentString() + " NOX-Android/1.0");
 
         webView.setBackgroundColor(Color.rgb(7, 9, 15));
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                if (request.isForMainFrame()) {
+                    Log.e(TAG, "WebView main frame error: " + error.getDescription());
+                }
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onPermissionRequest(final PermissionRequest request) {
